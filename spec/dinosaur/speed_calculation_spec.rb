4c43d4d3646258ce5c10c27d317162f1bfeb39d5
Marco Damaceno <maadamaceno@gmail.com>
2021-09-03 19:42:43 -0300

    refactor: use instance of DbContext

--- spec/dinosaur/speed_calculation_spec.rb
+++ spec/dinosaur/speed_calculation_spec.rb
@@ -4,9 +4,19 @@ require 'spec_helper'
 require_relative '../support/db_context_mock'
 
 RSpec.describe Dinosaur::SpeedCalculation do
+  let(:instance_db_context) { Dinosaur::DbContext.new }
+  let(:instance_db_context_mock) { DbContextMock.new }
+
+  before do
+    allow(instance_db_context).to receive(:stances)
+      .and_return(instance_db_context_mock.stances)
+    allow(instance_db_context).to receive(:diets)
+      .and_return(instance_db_context_mock.diets)
+  end
+
   describe '#faster' do
     it 'returns a list of dinosaurs from the faster to the slower' do
-      speed_calculation = described_class.new(db_context: DbContextMock.new)
+      speed_calculation = described_class.new(db_context: instance_db_context)
       dinosaurs = speed_calculation.faster
 
       dinosaurs.each_with_index do |dino, index|
